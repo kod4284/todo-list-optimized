679947dd2594f53e83185369c31337f3c4b9747d
Daewoong <kodw4284@gmail.com>
2020-03-09 19:54:59 +0900

    Render TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import TodoTemplate from './components/TodoTemplate';
-import TodoInsert from './components/TodoInsert';
+import TodoList from '@/components/TodoList';
+import TodoTemplate from '@/components/TodoTemplate';
+import TodoInsert from '@/components/TodoInsert';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
